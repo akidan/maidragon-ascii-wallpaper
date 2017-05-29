f93620712321f98e6bac2d9157a69d709a2810f1
Yue <zhiyue.wang@rakuten.com>
2017-05-29 20:46:27 +0900

    fix hair after line 57; todo: amount over 10

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ amount=3
 spacing=20
 maxtrim=100
 
-amount=sys.argv[1] if len(sys.argv)>1 and sys.argv[1].isdigit() else amount
-spacing=sys.argv[2] if len(sys.argv)>2 and sys.argv[2].isdigit() else spacing
+amount=int(sys.argv[1]) if len(sys.argv)>1 and sys.argv[1].isdigit() else amount
+spacing=int(sys.argv[2]) if len(sys.argv)>2 and sys.argv[2].isdigit() else spacing
 
 with codecs.open(input,'r','utf-8') as f1:
     #print(f1.read().encode('utf-8'))
@@ -19,9 +19,24 @@ with codecs.open(output,'w','utf-8') as f2:
     f2.write("")
 for i in readline:
     maxtrim = len(i)-len(i.lstrip()) if len(i)-len(i.lstrip()) > maxtrim else maxtrim
+line=0
+hair=56
 for i in readline:
-  with codecs.open(output,'a','utf-8') as f2:
-    i=i[0:(len(i)-1)]+maxtrim * ' ' + i[len(i)-1]
-    trimlen = len(i) - len(i.lstrip())
-    f2.write(trimlen*' '+i[trimlen:(trimlen+int(spacing))]*int(amount)+i[trimlen:].rstrip()+i[len(i)-1])
-## todo: after LINE57, fix the hair
+    with codecs.open(output,'a','utf-8') as f2:
+        i=i[0:(len(i)-1)]+maxtrim * ' ' + i[len(i)-1:]
+        trimlen = len(i) - len(i.lstrip())
+        if line<hair:
+            f2.write(trimlen*' '+i[trimlen:(trimlen+int(spacing))]*amount+i[trimlen:].rstrip()+i[len(i)-1:])
+        else:
+            headno=1
+            content=i[0:(len(i)-1)]+amount*len(i)*' ' + i[len(i)-1:]
+            i=i.lstrip()
+            while headno <= amount:
+                pointer=0
+                while pointer < len(i):
+                    if content[trimlen+headno*spacing+pointer] is ' ' and i[pointer:pointer+1] is not ' ':
+                        content=content[0:trimlen+headno*spacing+pointer-1]+i[pointer:pointer+1]+content[trimlen+headno*spacing+pointer:]
+                    pointer+=1
+                headno+=1
+            f2.write(content.rstrip()+i[len(i)-1:])
+        line+=1;
