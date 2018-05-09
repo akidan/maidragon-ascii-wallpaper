b84c02d75b75ee30b83a37b835fd1a4818db056d
zhiyue.wang <zhiyue.wang@rakuten.com>
2018-05-09 19:27:21 +0900

    add color feature but still need manual operation

--- main_color.py
+++ main_color.py
@@ -33,19 +33,32 @@ for i in readline:
             printline = trimlen*' '
             for j in range(0,amount):
                 printline =  printline + color_prefix + str(color_hash[j%len(color_hash)]) + color_suffix + i[trimlen:(trimlen+int(spacing))]
-            f2.write(printline+color_prefix + str(color_hash[amount%len(color_hash)]) + color_suffix+i[trimlen:].rstrip()+i[len(i)-1:])
-            f2.write('\x1b[0m')
+            f2.write(printline+color_prefix + str(color_hash[amount%len(color_hash)]) + color_suffix+i[trimlen:].rstrip()+i[len(i)-1:-1]+'\x1b[0m\n')
         else:
             headno=1
             content=i[0:(len(i)-1)]+amount*len(i)*' ' + i[len(i)-1:]
             i=i.lstrip()
+            color_matrix = [0] * (len(content) * headno)
             while headno <= amount:
+                # create a matrix for color mapping
+                print(content)
+                for j in range(0,len(content)):
+                    if content[j] != ' ' or (content[j] == ' ' and color_matrix[j]>0):
+                        color_matrix[j] = color_matrix[j]+1
+
                 pointer=0
                 while pointer < len(i)-1:
                     if content[trimlen+headno*spacing+pointer] == ' ' and i[pointer] != ' ':
                         content=content[0:trimlen+headno*spacing+pointer-1]+i[pointer]+content[trimlen+headno*spacing+pointer:]
                     pointer+=1
                 headno+=1
-            f2.write(content.rstrip()+i[len(i)-1:])
-            f2.write('\x1b[0m')
+            #print(color_matrix[:200])
+            content = content.rstrip()+i[len(i)-1:-1]
+            newContent = ''
+            for j in range(0,len(content)):
+                if (content[j] == ' '):
+                    newContent = newContent + content[j]
+                else:
+                    newContent = newContent + color_prefix + str(color_hash[(amount - color_matrix[j])%len(color_hash)]) + color_suffix + content[j]
+            f2.write(newContent+'\x1b[0m\n')
         line+=1;
